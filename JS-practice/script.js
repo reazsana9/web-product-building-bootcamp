c361e67fe2159d349c674282f753728c33850f4c
reazsana9 <reazsana9@gmail.com>
2026-09-17 21:51:33 +0600

    js arrow function added

---
+++ JS-practice/script.js
@@ -0,0 +1,113 @@
+// let x = 10;
+// console.log(x);
+
+// variables
+
+// let FirstName = "";
+// let age = 20;
+// let isActive = false;
+
+// const yourBirthDate = "2 Feb 2002";
+
+// var price = 100;
+// FirstName = "Mr. Jodu"
+
+// console.log("This is a console log");
+// console.log(10);
+// console.log(price);
+
+// let firstName = "Mr."
+// let lastName = "Jodu Mia";
+// let role = "Student"
+// let price = 100;
+
+// console.log("My name is ", firstName, lastName, "And i am a", role);
+
+// console.log(`My name is ${firstName} ${lastName} And i am a ${role}`);
+
+// const priceWithVat = `The price with vat = ${price + 15}`
+
+// console.log(priceWithVat);
+
+// Conditional operation:
+
+// if, else if, else
+
+// let price = 100;
+// let vat = 15;
+
+// if (price >= 100) {
+//     let priceWithvat = price + vat
+//     console.log(`etar upor vat apply hobe and price with vat ${priceWithvat}`);
+
+// } else {
+//     console.log(`Etar upor vat nei and price ${price}`);
+
+// }
+
+// let orderPrice = 1000;
+
+// if (orderPrice > 999) {
+//     console.log("Free Delivery")
+// } else{
+//     console.log(`Delivery Charge Applicable`)
+// }
+
+// let trafficLight = "Yellow";
+
+// if (trafficLight == "Red") {
+//     console.log("Stop!")
+// } else if (trafficLight == "Yellow") {
+//     console.log("Get Ready")
+// } else {
+//     console.log("GO!")
+// }
+
+// let oshukh = true;
+// let rain = true;
+
+// if (oshukh == true && rain == true) {
+//     console.log("school cancel")
+// } else {
+//     console.log("School jabo")
+
+// }
+
+// Function
+
+// function sum(n1, n2 = 0) {
+//     console.log(`n2 er value ${n2}`)
+//     let total = n1 + n2;
+//     console.log(total);
+// }
+// sum(5);
+
+// const sum = (n1, n2 = 0) => {
+//     let total = n1 + n2;
+//     console.log(total);
+// }
+
+// function sum() {
+//     return 5 + 10;
+// }
+
+// const sum =()=> 5 +15
+
+// const total= sum()
+// console.log(total)
+
+// const calculatePrice =(item1, item2) =>{
+// const total = item1 + item2;
+// console. log(total)
+
+// }
+
+// const priceResult = calculatePrice(150, 200);
+// console.log(priceResult);
+
+const calDis = (buyingPrice, discount) =>{
+const afterDis = buyingPrice - discount
+return afterDis;
+}
+const discountResult = calDis(priceResult, 15);
+console. log ("After discount price is ", discountResult)
